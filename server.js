d014879eb3b1287bc9cbc407a6bb97a079a37d5b
Mateus C <mateus_mcg@icloud.com>
2016-08-29 21:08:04 -0300

    Fix bower and static files

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express'),
     app = express();
 
 app.use(express.static(__dirname + "/public"));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 // Initialize the app.
 var server = app.listen(process.env.PORT || 8080, function () {
