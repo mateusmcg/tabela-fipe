e72010c7b52a0ad3b4664c5abe12ec93f99cb8c2
Mateus C <mateus_mcg@icloud.com>
2016-09-01 01:23:46 -0300

    Added typeahead - partial

--- public/app.js
+++ public/app.js
@@ -1,17 +1,28 @@
 'use strict';
 
-var app = angular.module('app', []);
+var app = angular.module('app', ['ui.bootstrap']);
 
-app.config([function(){
-    
+app.constant('baseApiUrl', 'http://fipeapi.appspot.com/api/1/');
+
+app.config([function () {
+
 }]);
 
-app.run([function(){
-    
+app.run([function () {
+
 }]);
 
-app.controller('mainCtrl', ['$log', function ($log) {
+app.controller('mainCtrl', ['$log', 'baseApiUrl', '$http', function ($log, baseApiUrl, $http) {
     $log.debug('Running main controller');
-    
+
     var vm = this;
+    vm.marcas = [];
+    
+    getMarcas();
+
+    function getMarcas() {
+        $http.get(baseApiUrl + 'carros/marcas.json').then(function (result) {
+            vm.marcas = result.data.map(function (marca) { return marca.fipe_name });
+        })
+    }
 }])
